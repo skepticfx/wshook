d76214c3fca608245cb3acc7edc0398c13ac2ee1
skepticfx <ahamed.nafeez@gmail.com>
2015-03-17 01:26:19 -0700

    add wsHook. onsend and onmessage events

---
+++ example.js
@@ -0,0 +1,18 @@
+// Load wsHook.js
+
+// Define the 'onSend' and 'onMessage' Event Listeners from wsHook
+
+wsHook.onSend = function(e) {
+    console.log("Message Sent to " + e.url + " : " + e.data);
+}
+wsHook.onMessage = function(e) {
+    console.log("Message recieved from " + e.url + " : " + e.data);
+}
+
+
+
+var wsClient = new WebSocket("wss://echo.websocket.org");
+
+wsClient.onopen = function() {
+    wsClient.send("Echo this");
+}
